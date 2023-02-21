5dcae525b7b840cd0ee65de465542b778a33e73d
Thomas Schuehly <thomas.schuehly@outlook.com>
2023-02-21 22:40:32 +0100

    jitpack spring boot plugin not found

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.2"
+	id("org.springframework.boot") version "3.0.1"
 	id("io.spring.dependency-management") version "1.1.0"
 	id("maven-publish")
 	kotlin("jvm") version "1.7.22"
