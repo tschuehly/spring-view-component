abab08c74fe2ca3a34f376f2229aeda5208048f5
Thomas Schuehly <thomas.schuehly@outlook.com>
2023-09-02 23:02:07 +0200

    added kte example

---
+++ examples/kte-example/src/main/kotlin/de/tschuehly/jteviewcomponentdemo/JteViewComponentDemoApplication.kt
@@ -0,0 +1,12 @@
+package de.tschuehly.jteviewcomponentdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JteViewComponentDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<JteViewComponentDemoApplication>(*args)
+}
+
---
+++ examples/kte-example/src/main/kotlin/de/tschuehly/jteviewcomponentdemo/web/index/IndexViewComponent.kt
@@ -0,0 +1,9 @@
+package de.tschuehly.jteviewcomponentdemo.web.index
+
+import de.tschuehly.spring.viewcomponent.core.component.ViewComponent
+import de.tschuehly.spring.viewcomponent.jte.EmptyViewContext
+
+@ViewComponent
+class IndexViewComponent {
+    fun render() = EmptyViewContext()
+}
