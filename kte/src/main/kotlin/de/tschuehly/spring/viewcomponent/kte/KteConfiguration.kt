18840a6b57d1b396379559dac8c443c0be62a623
Christoph Hagelkruys <christophhagelkruys@MBPvonChristoph.home>
2024-08-21 18:30:59 +0200

    fix(kte): kte now working without development mode on

--- kte/src/main/kotlin/de/tschuehly/spring/viewcomponent/kte/KteConfiguration.kt
+++ kte/src/main/kotlin/de/tschuehly/spring/viewcomponent/kte/KteConfiguration.kt
@@ -1,19 +1,24 @@
 package de.tschuehly.spring.viewcomponent.kte
+
 import de.tschuehly.spring.viewcomponent.core.component.ViewComponentProperties
 import gg.jte.CodeResolver
 import gg.jte.ContentType
 import gg.jte.TemplateEngine
 import gg.jte.resolve.DirectoryCodeResolver
 import gg.jte.springframework.boot.autoconfigure.JteProperties
+import org.springframework.beans.factory.BeanClassLoaderAware
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.*
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import java.nio.file.FileSystems
 import java.nio.file.Path
 import java.nio.file.Paths
 
 @Configuration
-class KteConfiguration {
+class KteConfiguration : BeanClassLoaderAware {
+    var classLoader: ClassLoader? = null;
 
     @Bean
     fun kteViewContextAspect(templateEngine: TemplateEngine, jteProperties: JteProperties): KteViewContextAspect {
@@ -25,31 +30,31 @@ class KteConfiguration {
     fun jteTemplateEngine(viewComponentProperties: ViewComponentProperties,applicationContext: ApplicationContext): TemplateEngine {
         if (!viewComponentProperties.localDevelopment) {
             return TemplateEngine.createPrecompiled(
-                /* classDirectory = */ Path.of(this.javaClass.classLoader.getResource("").toURI()),
+                /* classDirectory = */ Path.of(classLoader?.getResource("")?.toURI() ?: throw RuntimeException("ClassLoader is null")),
                 /* contentType = */ ContentType.Html,
-                /* parentClassLoader = */ this.javaClass.classLoader
+                /* parentClassLoader = */ classLoader
             )
         }
-        val split = getViewComponentRootPath(viewComponentProperties).split("/").toTypedArray()
+        val split = viewComponentProperties.viewComponentRoot.split("/").toTypedArray()
         val codeResolver: CodeResolver = DirectoryCodeResolver(FileSystems.getDefault().getPath("", *split))
         return TemplateEngine.create(
             codeResolver,
             Paths.get("jte-classes"),
             ContentType.Html,
-            Thread.currentThread().contextClassLoader
+            classLoader
         )
     }
 
     @Bean
     @Primary
-    fun jteProperties(viewComponentProperties: ViewComponentProperties) = JteProperties().also {
-        it.templateSuffix = ".kte"
-        it.templateLocation = getViewComponentRootPath(viewComponentProperties)
-        it.isDevelopmentMode = viewComponentProperties.localDevelopment
+    fun jteProperties(viewComponentProperties: ViewComponentProperties) =
+        JteProperties().also {
+            it.templateSuffix = ".kte"
+            it.templateLocation = viewComponentProperties.viewComponentRoot
+            it.isDevelopmentMode = viewComponentProperties.localDevelopment
+        }
+
+    override fun setBeanClassLoader(classLoader: ClassLoader) {
+        this.classLoader = classLoader
     }
-
-    private fun getViewComponentRootPath(viewComponentProperties: ViewComponentProperties): String =
-        if (viewComponentProperties.viewComponentRoot == "src/main/java") "src/main/kotlin" else viewComponentProperties.viewComponentRoot
-
-
 }
